59f9710f49300c09fe12716eb085be07820e26f8
aroran1 <nidhigulati2001@gmail.com>
2021-09-30 12:51:26 +0100

    added libraries and page setup

---
+++ app2/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const App = () => {
+  return(
+    <div>
+      <div style={{
+        margin:"10px",
+        padding:"10px",
+        textAlign:"center",
+        backgroundColor:"cyan"
+      }}>
+        <h1>App2</h1>
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
